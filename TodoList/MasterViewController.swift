ee317e9041e27eb29e925c26f397f7280062e45f
Bennett Cheung <bennettcheung@yahoo.com>
2018-09-05 15:44:19 -0700

    Added fingerprint authentication

--- TodoList/MasterViewController.swift
+++ TodoList/MasterViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import CoreData
+import LocalAuthentication
 
 class MasterViewController: UITableViewController, NSFetchedResultsControllerDelegate {
 
@@ -21,14 +22,17 @@ class MasterViewController: UITableViewController, NSFetchedResultsControllerDel
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
     navigationItem.leftBarButtonItem = editButtonItem
+    editButtonItem.isEnabled = false
 
     let addButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(insertNewObject(_:)))
     navigationItem.rightBarButtonItem = addButton
+    addButton.isEnabled = false
     if let split = splitViewController {
         let controllers = split.viewControllers
         detailViewController = (controllers[controllers.count-1] as! UINavigationController).topViewController as? DetailViewController
     }
     setupUserDefaultValues()
+    authenticationWithTouchID()
   }
 
   override func viewWillAppear(_ animated: Bool) {
@@ -257,6 +261,15 @@ class MasterViewController: UITableViewController, NSFetchedResultsControllerDel
        tableView.reloadData()
    }
    */
+  // Mark: UI related functions
+
+  func unhideUI() {
+      DispatchQueue.main.async{
+      self.tableView.isHidden = false
+      self.navigationItem.leftBarButtonItem?.isEnabled = true
+      self.navigationItem.rightBarButtonItem?.isEnabled = true
+    }
+  }
 
 }
 
@@ -265,3 +278,129 @@ extension MasterViewController: DetailViewControllerDelegate{
     saveContext()
   }
 }
+
+extension MasterViewController {
+  
+  func authenticationWithTouchID() {
+    let localAuthenticationContext = LAContext()
+    localAuthenticationContext.localizedFallbackTitle = "Login/Password"
+    
+    var authError: NSError?
+    let reasonString = "To access the toDo Data"
+    
+    if localAuthenticationContext.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &authError) {
+      
+      localAuthenticationContext.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reasonString) { success, evaluateError in
+        
+        if success {
+          
+          //TODO: User authenticated successfully, take appropriate action
+          
+          self.unhideUI()
+
+          print("user authentication successful")
+          
+        } else {
+          //TODO: User did not authenticate successfully, look at error and take appropriate action
+          print("user authentication failed")
+          guard let error = evaluateError else {
+            return
+          }
+          
+          print(self.evaluateAuthenticationPolicyMessageForLA(errorCode: error._code))
+          
+          //TODO: If you have choosen the 'Fallback authentication mechanism selected' (LAError.userFallback). Handle gracefully
+          if (error._code == LAError.userFallback.rawValue)
+          {
+            
+          }
+          else{
+            self.authenticationWithTouchID()
+          }
+          
+        }
+      }
+    } else {
+      
+      guard let error = authError else {
+        return
+      }
+      //TODO: Show appropriate alert if biometry/TouchID/FaceID is lockout or not enrolled
+      print(self.evaluateAuthenticationPolicyMessageForLA(errorCode: error.code))
+    }
+  }
+  
+  func evaluatePolicyFailErrorMessageForLA(errorCode: Int) -> String {
+    var message = ""
+    if #available(iOS 11.0, macOS 10.13, *) {
+      switch errorCode {
+      case LAError.biometryNotAvailable.rawValue:
+        message = "Authentication could not start because the device does not support biometric authentication."
+        
+      case LAError.biometryLockout.rawValue:
+        message = "Authentication could not continue because the user has been locked out of biometric authentication, due to failing authentication too many times."
+        
+      case LAError.biometryNotEnrolled.rawValue:
+        message = "Authentication could not start because the user has not enrolled in biometric authentication."
+        
+      default:
+        message = "Did not find error code on LAError object"
+      }
+    } else {
+      switch errorCode {
+      case LAError.touchIDLockout.rawValue:
+        message = "Too many failed attempts."
+        
+      case LAError.touchIDNotAvailable.rawValue:
+        message = "TouchID is not available on the device"
+        
+      case LAError.touchIDNotEnrolled.rawValue:
+        message = "TouchID is not enrolled on the device"
+        
+      default:
+        message = "Did not find error code on LAError object"
+      }
+    }
+    
+    return message;
+  }
+  
+  func evaluateAuthenticationPolicyMessageForLA(errorCode: Int) -> String {
+    
+    var message = ""
+    
+    switch errorCode {
+      
+    case LAError.authenticationFailed.rawValue:
+      message = "The user failed to provide valid credentials"
+      
+    case LAError.appCancel.rawValue:
+      message = "Authentication was cancelled by application"
+      
+    case LAError.invalidContext.rawValue:
+      message = "The context is invalid"
+      
+    case LAError.notInteractive.rawValue:
+      message = "Not interactive"
+      
+    case LAError.passcodeNotSet.rawValue:
+      message = "Passcode is not set on the device"
+      
+    case LAError.systemCancel.rawValue:
+      message = "Authentication was cancelled by the system"
+      
+    case LAError.userCancel.rawValue:
+      message = "The user did cancel"
+      
+    case LAError.userFallback.rawValue:
+      message = "The user chose to use the fallback"
+      
+    default:
+      message = evaluatePolicyFailErrorMessageForLA(errorCode: errorCode)
+    }
+    
+    return message
+  }
+}
+
+
